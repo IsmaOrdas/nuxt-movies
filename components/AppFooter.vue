363905e911db0d5707573ec8cff35a1de22debeb
Ismael Ordás <ismael.ordas@gmail.com>
2023-05-02 10:08:34 +0200

    Update AppFooter.vue

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,7 +2,7 @@
     <footer class="app-footer">
 			<div class="flex justify-center items-center h-full">
 				<p class="text-center">
-					Made with Nuxt 3 and UnoCSS. Data powered by <a href="https://www.themoviedb.org" target="_blank" rel="noopener noreferrer">TMDb API</a>
+					Made with Nuxt 3 and UnoCSS. Data powered by <a href="https://www.themoviedb.org" target="_blank" rel="noopener noreferrer">TMDB API</a>
 				</p>
 			</div>
     </footer>
